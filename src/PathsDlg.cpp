9b0c71269a650bb0960bd6490cb6997f3a9ca6eb
Tobias WEBER <tweber@ill.fr>
2021-02-10 14:29:57 +0100

    cosmetic

--- src/PathsDlg.cpp
+++ src/PathsDlg.cpp
@@ -3,7 +3,6 @@
  * @author Tobias Weber <tweber@ill.fr>
  * @date feb-2021
  * @license GPLv3, see 'LICENSE' file
- * @desc The present version was forked on 8-Nov-2018 from my privately developed "magtools" project (https://github.com/t-weber/magtools).
  */
 
 #include <QtCore/QSettings>
