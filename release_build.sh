b57dd5d38cc01e2f8440b554cb24b9da9339aa8c
Tobias WEBER <tweber@ill.fr>
2021-04-08 16:54:55 +0200

    small build script

---
+++ release_build.sh
@@ -0,0 +1,21 @@
+#
+# @author Tobias Weber <tweber@ill.fr>
+# @date apr-2021
+# @license GPLv3, see 'LICENSE' file
+#
+
+rm -rfv build
+mkdir build
+cd build
+
+if ! cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-O2" -DCMAKE_VERBOSE_MAKEFILE=False ..; then
+	echo -e "cmake failed."
+	exit -1
+fi
+
+if ! make -j4; then
+	echo -e "make failed."
+	exit -1
+fi
+
+strip -v paths
