05d8352528cbddf69f2155c1a6a9aa9526d9d3a6
Tobias Weber <tobias.weber@tum.de>
2021-05-24 11:27:48 +0200

    continued with mingw setup script

--- setup/mingw/mk.sh
+++ setup/mingw/mk.sh
@@ -3,20 +3,47 @@
 # creates a mingw distribution
 # @author Tobias Weber <tweber@ill.fr>
 # @date may-2021
-# @license GPLv2
+# @license GPLv3, see 'LICENSE' file
 #
 
-# defines
+use_lapacke=0
+
 APPNAME="TASPaths"
 APPDIRNAME="${APPNAME}"
+
+
+# third-party libraries
+EXT_LIBS=( \
+	Qt5Core.dll Qt5Gui.dll Qt5Widgets.dll Qt5OpenGL.dll Qt5DBus.dll Qt5PrintSupport.dll Qt5Svg.dll \
+	libboost_system-x64.dll libboost_filesystem-x64.dll \
+	libstdc++-6.dll libwinpthread-1.dll libglib-2.0-0.dll libgcc_s_sjlj-1.dll libgcc_s_seh-1.dll \
+	libbz2-1.dll zlib1.dll \
+	libpng16-16.dll \
+	libfreetype-6.dll \
+	libpcre2-16-0.dll libpcre-1.dll libssp-0.dll \
+	libharfbuzz-0.dll \
+	iconv.dll libintl-8.dll \
+)
+
+# lapack(e) libraries
+if [ $create_appdir -ne 0 ]; then
+	EXT_LIBS+=( \
+		liblapack.dll liblapacke.dll libblas.dll \
+		libgfortran-5.dll libquadmath-0.dll \
+	)
+fi
+
+# qt plugins
+QT_PLUGINS=( \
+	platforms/*.dll \
+	styles/*.dll \
+)
 
 # create directories
 mkdir -p ${APPDIRNAME}
 mkdir -p ${APPDIRNAME}/res
 mkdir -p ${APPDIRNAME}/qt_plugins/platforms/
 mkdir -p ${APPDIRNAME}/qt_plugins/styles/
-mkdir -p ${APPDIRNAME}/qt_plugins/iconengines/
-mkdir -p ${APPDIRNAME}/qt_plugins/imageformats
 
 # copy program files
 cp -v build/*.exe          ${APPDIRNAME}/
@@ -25,49 +52,20 @@ cp -v AUTHORS              ${APPDIRNAME}/
 cp -v LICENSE              ${APPDIRNAME}/
 cp -rv 3rdparty_licenses/  ${APPDIRNAME}/
 
-# create qt config file
-echo -e "[Paths]\nPlugins = qt_plugins\n" > ${APPDIRNAME}/qt.conf
 
 # copy third-party libraries
-cp -v /usr/x86_64-w64-mingw32/sys-root/mingw/bin/Qt5Core.dll                  ${APPDIRNAME}/
-cp -v /usr/x86_64-w64-mingw32/sys-root/mingw/bin/Qt5Gui.dll                   ${APPDIRNAME}/
-cp -v /usr/x86_64-w64-mingw32/sys-root/mingw/bin/Qt5Widgets.dll               ${APPDIRNAME}/
-cp -v /usr/x86_64-w64-mingw32/sys-root/mingw/bin/Qt5OpenGL.dll                ${APPDIRNAME}/
-cp -v /usr/x86_64-w64-mingw32/sys-root/mingw/bin/Qt5DBus.dll                  ${APPDIRNAME}/
-cp -v /usr/x86_64-w64-mingw32/sys-root/mingw/bin/Qt5PrintSupport.dll          ${APPDIRNAME}/
-cp -v /usr/x86_64-w64-mingw32/sys-root/mingw/bin/Qt5Svg.dll                   ${APPDIRNAME}/
-
-cp -v /usr/x86_64-w64-mingw32/sys-root/mingw/bin/libboost_system-x64.dll      ${APPDIRNAME}/
-cp -v /usr/x86_64-w64-mingw32/sys-root/mingw/bin/libboost_filesystem-x64.dll  ${APPDIRNAME}/
-
-cp -v /usr/x86_64-w64-mingw32/sys-root/mingw/bin/libstdc++-6.dll              ${APPDIRNAME}/
-cp -v /usr/x86_64-w64-mingw32/sys-root/mingw/bin/libwinpthread-1.dll          ${APPDIRNAME}/
-cp -v /usr/x86_64-w64-mingw32/sys-root/mingw/bin/libglib-2.0-0.dll            ${APPDIRNAME}/
-cp -v /usr/x86_64-w64-mingw32/sys-root/mingw/bin/libgcc_s_sjlj-1.dll          ${APPDIRNAME}/
-cp -v /usr/x86_64-w64-mingw32/sys-root/mingw/bin/libgcc_s_seh-1.dll           ${APPDIRNAME}/
-
-cp -v /usr/x86_64-w64-mingw32/sys-root/mingw/bin/libbz2-1.dll                 ${APPDIRNAME}/
-cp -v /usr/x86_64-w64-mingw32/sys-root/mingw/bin/zlib1.dll                    ${APPDIRNAME}/
-cp -v /usr/x86_64-w64-mingw32/sys-root/mingw/bin/libpng16-16.dll              ${APPDIRNAME}/
-cp -v /usr/x86_64-w64-mingw32/sys-root/mingw/bin/iconv.dll                    ${APPDIRNAME}/
-cp -v /usr/x86_64-w64-mingw32/sys-root/mingw/bin/libpcre2-16-0.dll            ${APPDIRNAME}/
-cp -v /usr/x86_64-w64-mingw32/sys-root/mingw/bin/libharfbuzz-0.dll            ${APPDIRNAME}/
-cp -v /usr/x86_64-w64-mingw32/sys-root/mingw/bin/libintl-8.dll                ${APPDIRNAME}/
-cp -v /usr/x86_64-w64-mingw32/sys-root/mingw/bin/libpcre-1.dll                ${APPDIRNAME}/
-cp -v /usr/x86_64-w64-mingw32/sys-root/mingw/bin/libssp-0.dll                 ${APPDIRNAME}/
-cp -v /usr/x86_64-w64-mingw32/sys-root/mingw/bin/libfreetype-6.dll            ${APPDIRNAME}/
-
-#cp -v /usr/x86_64-w64-mingw32/sys-root/mingw/bin/liblapack.dll               ${APPDIRNAME}/
-#cp -v /usr/x86_64-w64-mingw32/sys-root/mingw/bin/liblapacke.dll              ${APPDIRNAME}/
-#cp -v /usr/x86_64-w64-mingw32/sys-root/mingw/bin/libblas.dll                 ${APPDIRNAME}/
-#cp -v /usr/x86_64-w64-mingw32/sys-root/mingw/bin/libquadmath-0.dll           ${APPDIRNAME}/
-#cp -v /usr/x86_64-w64-mingw32/sys-root/mingw/bin/libgfortran-5.dll           ${APPDIRNAME}/
+for THELIB in ${EXT_LIBS[@]}; do
+	cp -v /usr/x86_64-w64-mingw32/sys-root/mingw/bin/${THELIB} ${APPDIRNAME}/
+done
 
 # copy qt plugins
-cp -v /usr/x86_64-w64-mingw32/sys-root/mingw/lib/qt5/plugins/platforms/*.dll            ${APPDIRNAME}/qt_plugins/platforms/
-cp -v /usr/x86_64-w64-mingw32/sys-root/mingw/lib/qt5/plugins/styles/*.dll               ${APPDIRNAME}/qt_plugins/styles/
-#cp -v /usr/x86_64-w64-mingw32/sys-root/mingw/lib/qt5/plugins/iconengines/qsvgicon.dll  ${APPDIRNAME}/qt_plugins/iconengines/
-#cp -v /usr/x86_64-w64-mingw32/sys-root/mingw/lib/qt5/plugins/imageformats/qsvg.dll     ${APPDIRNAME}/qt_plugins/imageformats/
+for THELIB in ${QT_PLUGINS[@]}; do
+	LIBDIRNAME=$(dirname ${THELIB})
+	cp -v /usr/x86_64-w64-mingw32/sys-root/mingw/lib/qt5/plugins/${THELIB} ${APPDIRNAME}/qt_plugins/${LIBDIRNAME}
+done
+
+# create qt config file
+echo -e "[Paths]\n\tPlugins = qt_plugins\n" > ${APPDIRNAME}/qt.conf
 
 # stripping
 find ${APPDIRNAME} -type f \( -name "*.exe" -o -name "*.dll" \) -exec strip -v {} \;
