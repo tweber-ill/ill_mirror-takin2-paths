e7941ea666ecf89d5837cfc2631720e32db4b813
Tobias Weber <tobias.weber@tum.de>
2021-06-07 00:25:26 +0200

    combined progress signal return values

--- src/core/PathsBuilder.h
+++ src/core/PathsBuilder.h
@@ -55,8 +55,28 @@ private:
 	const InstrumentSpace *m_instrspace{};
 	const t_real* m_sensesCCW{nullptr};
 
+	// and-combine return values for calculation progress signal
+	struct combine_sigret
+	{
+		using result_type = bool;
+
+		template<class t_iter>
+		bool operator()(t_iter begin, t_iter end) const
+		{
+			result_type ret = true;
+
+			for(t_iter iter = begin; iter != end; iter = std::next(iter))
+				ret = ret && *iter;
+
+			return ret;
+		}
+	};
+
 	// calculation progress signal
-	using t_sig_progress = boost::signals2::signal<bool(bool start, bool end, t_real progress)>;
+	using t_sig_progress =
+		boost::signals2::signal<
+			bool(bool start, bool end, t_real progress),
+			combine_sigret>;
 	std::shared_ptr<t_sig_progress> m_sigProgress;
 
 	geo::Image<std::uint8_t> m_img;
